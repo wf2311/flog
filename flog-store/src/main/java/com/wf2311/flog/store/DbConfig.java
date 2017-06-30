11076478555db85b08897c111111d7723044e0ca
wf2311 <OS4567st>
2017-06-30 17:55:12 +0800

    flog-store

---
+++ flog-store/src/main/java/com/wf2311/flog/store/DbConfig.java
@@ -0,0 +1,11 @@
+package com.wf2311.flog.store;
+
+/**
+ * @author wf2311
+ */
+public class DbConfig {
+    private String driverName;
+    private String jdbcUrl;
+    private String userName;
+    private String password;
+}
